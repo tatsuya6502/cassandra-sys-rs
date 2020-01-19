3f45d342f12e9843230dc0a1b961de34dd01027a
Tatsuya Kawano <tatsuya@hibaridb.org>
2020-01-19 15:15:08 +0800

    Add static linking option to build.rs
    If CASSANDRA_SYS_STATIC_LINK environment variable is defined at the
    build time, build.rs generates linker flags for static linking.
    
    Signed-off-by: Tatsuya Kawano <tatsuya@hibaridb.org>

--- build.rs
+++ build.rs
@@ -5,15 +5,26 @@ fn main() {
         }
     }
 
-    println!("cargo:rustc-flags=-l dylib=cassandra");
-    println!("cargo:rustc-flags=-l dylib=crypto");
-    println!("cargo:rustc-flags=-l dylib=ssl");
-    println!("cargo:rustc-flags=-l dylib=stdc++");
-    println!("cargo:rustc-flags=-l dylib=uv");
-    println!("cargo:rustc-link-search={}", "/usr/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-search={}", "/usr/local/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-search={}", "/usr/local/lib64");
-    println!("cargo:rustc-link-search={}", "/usr/local/lib");
-    println!("cargo:rustc-link-search={}", "/usr/lib64/");
-    println!("cargo:rustc-link-search={}", "/usr/lib/");
+    if let Some(_) = option_env!("CASSANDRA_SYS_STATIC_LINK") {
+        println!("cargo:rustc-link-lib=static=cassandra_static");
+        println!("cargo:rustc-link-lib=static=crypto");
+        println!("cargo:rustc-link-lib=static=ssl");
+        println!("cargo:rustc-link-lib=static=stdc++");
+        println!("cargo:rustc-link-lib=static=uv");
+        println!("cargo:rustc-link-lib=static=z");
+        println!("cargo:rustc-link-search={}", "/usr/lib");
+        println!("cargo:rustc-link-search={}", "/lib");
+    } else {
+        println!("cargo:rustc-flags=-l dylib=cassandra");
+        println!("cargo:rustc-flags=-l dylib=crypto");
+        println!("cargo:rustc-flags=-l dylib=ssl");
+        println!("cargo:rustc-flags=-l dylib=stdc++");
+        println!("cargo:rustc-flags=-l dylib=uv");
+        println!("cargo:rustc-link-search={}", "/usr/lib/x86_64-linux-gnu");
+        println!("cargo:rustc-link-search={}", "/usr/local/lib/x86_64-linux-gnu");
+        println!("cargo:rustc-link-search={}", "/usr/local/lib64");
+        println!("cargo:rustc-link-search={}", "/usr/local/lib");
+        println!("cargo:rustc-link-search={}", "/usr/lib64/");
+        println!("cargo:rustc-link-search={}", "/usr/lib/");
+    }
 }
